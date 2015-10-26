0c3b2900a55f6f92317a2c35d9e02a09931635f0
a5482pick <fordlandia1928@gmail.com>
2015-10-26 00:09:04 +0000

    Basic outline files.

---
+++ README.md
@@ -0,0 +1,1 @@
+An outline for using the storage feature of Chrome extensions is provided.
---
+++ caller.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+    document.getElementById('save').addEventListener('click', saveChanges);
+    document.getElementById('change1').addEventListener('click', makeChanges);
+    document.getElementById('change2').addEventListener('click', makeChanges2);  
+    document.getElementById('clear').addEventListener('click', clearMemory);
+    document.getElementById('inStore').addEventListener('click', alertData);
+});
+
+
+function clearMemory() {
+    chrome.storage.local.clear(function() {alert("Storage cleared.")});
+}
+
+
+function makeChanges() {
+    document.getElementById("area2").value = document.getElementById("area2").value + "red";
+}
+
+
+function makeChanges2() {
+    document.getElementById("area").value = document.getElementById("area").value + "blue";
+}
+
+
+function alertData()   {
+    chrome.storage.local.get(function(items) {      
+             
+        var values = "";
+        for (key in items) {
+                
+            values = values + " " + items[key];
+        }        
+        alert(values);
+    });
+};
+
+
+function saveChanges() {
+
+    var value1 = document.getElementById("area").value;
+    var value2 = document.getElementById("area2").value;
+    
+    if (!value1 || !value2) {
+        alert('Error: Enter text in both boxes.');
+        return;
+    }
+    
+    var dataObject = {'value1' : value1, 'value2' : value2};
+    
+    chrome.storage.local.set(dataObject, function() {
+        
+        chrome.storage.local.get(function(items) {      
+             
+            var values = [];
+            var i = 0;
+            for (key in items) {
+                
+                values[i] = items[key];
+               i++;
+            }        
+            alert("The entered data was: " + values[0] + " and " + values[1]);
+        });
+    });
+}
+
